5f77d1eeb47ca30c072318f0ea4ba4e97e4d250b
Samar Srivastava <samarsrivastava44@gmail.com>
2022-10-05 10:09:51 +0530

    imports fixed

--- daily_ohlc_4pm/ohlc_4pm/start_app.py
+++ daily_ohlc_4pm/ohlc_4pm/start_app.py
@@ -1,6 +1,6 @@
 import sys
 
-from Nifty_Scrappers.daily_ohlc_4pm.ohlc_4pm.scrapper import getOHLCData
+from daily_ohlc_4pm.ohlc_4pm.scrapper import getOHLCData
 from Nifty_Scrappers.daily_ohlc_4pm.helpers.database import sqlHandling
 from Nifty_Scrappers.daily_ohlc_4pm.helpers.constants import get_list_of_trading_holidays
 from Nifty_Scrappers.daily_ohlc_4pm.helpers.logger import return_logger
